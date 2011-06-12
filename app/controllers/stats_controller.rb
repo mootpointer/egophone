31997e172e667379f14ff3dd66868da108c284cb
Chelsea Robb <chelsea.robb@gmail.com>
2011-06-12 18:17:59 +1000

    Initial graph, no GMT offset

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,6 +3,10 @@ class StatsController < ApplicationController
   def index
     # This code is insanely slow.  
     direction = Person.direction.to_a
+    sorted_data = Message.time_of_day.sort.flatten
+    @graph_data = []
+    sorted_data.each_with_index{|count, index| @graph_data << count if index % 2 != 0}
+    
     @most_sent_to = direction.sort{|a,b| b['value']['out'] <=> a['value']['out']}.take(20)
     @most_received_from = direction.sort{|a,b| b['value']['in'] <=> a['value']['in']}.take(20)
     
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,3 +7,9 @@ $(document).ready(function(){
 	});
 	
 });
+
+function drawHistogram(data){
+	$('#search-results-graph').tufteBar({
+    data: data
+  });
+}
